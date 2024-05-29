4e253b540d2178fc21ada10347927fdede3d3e95
Nepsoul <nirumagar901@gmail.com>
2024-05-29 17:21:45 +0545

    chore: understanding of static routing

--- app/(dashboard)/settings/page.tsx
+++
@@ -1,5 +0,0 @@
-/*usign (dashboard)=> group routing */
-const SettingPage = () => {
-  return <div>Setting page</div>;
-};
-export default SettingPage;
---
+++ app/(dashboard)/todos/page.tsx
@@ -0,0 +1,8 @@
+/*usign (dashboard)=> group routing */
+/*static routing: aleady know the name of route */
+
+console.log(process.argv); //only accessible in node/ server
+const TodosPage = () => {
+  return <div>Todos page</div>;
+};
+export default TodosPage;
