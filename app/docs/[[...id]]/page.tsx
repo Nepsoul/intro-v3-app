9fddec30069ec92ecc95a2c232723692451dc407
Nepsoul <nirumagar901@gmail.com>
2024-05-31 05:06:41 +0545

    chore: making route inclusive feat. [[...id]]

--- app/docs/[[...id]]/page.tsx
+++ app/docs/[[...id]]/page.tsx
@@ -1,5 +1,6 @@
 /* - [...id] =>nested routing(i.e. localhost:3001/docs/anything/any/any/any), it renders same page, 
    - it does not include docs(url has to be docs/somthing)
+   - after [[...id]], it makes docs inclusive which include docs(e.g.localhost:3001/docs, shows this page)
 */
 
 const DocPage = () => {
