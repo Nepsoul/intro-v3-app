ced4dd2a1f746b63003b3b4729c61c95e7ec23e7
Nepsoul <nirumagar901@gmail.com>
2024-05-31 04:29:51 +0545

    chore: giving multiple parameter

--- app/docs/[id]/[title]/page.tsx
+++ app/docs/[id]/[title]/page.tsx
@@ -1,7 +1,13 @@
 /*Dyanmic routing, [id] makes dynamic route*/
 //implementing the params make the route name dynamic
 const DocsIdPage = ({ params }) => {
-  return <div>id {params.id}</div>;
+  //giving multiple parameter
+  return (
+    <div>
+      id {params.id}
+      {params.title}
+    </div>
+  );
 };
 
 export default DocsIdPage;
