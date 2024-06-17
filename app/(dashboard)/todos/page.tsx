15648d5c5a59dca09e5a1ba399e334a33f3aa9bf
Nepsoul <nirumagar901@gmail.com>
2024-06-17 09:41:16 +0545

    chore: fetching data with prisma

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,8 +1,22 @@
-/*usign (dashboard)=> group routing */
+/*using (dashboard)=> group routing */
 /*static routing: aleady know the name of route */
 
 console.log(process.argv); //only accessible in node/ server
-const TodosPage = () => {
-  return <div>Todos page</div>;
+
+import TodoList from "@/components/TodoList";
+import db from "@/utils/db";
+
+const getData = async () => {
+  const todos = await db.todo.findMany({});
+  console.log(todos);
+  return todos;
+};
+const TodosPage = async () => {
+  const todos = await getData();
+  return (
+    <div>
+      <TodoList todos={todos} />
+    </div>
+  );
 };
 export default TodosPage;
