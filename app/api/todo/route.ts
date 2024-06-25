b90846065653f7ac61ecf16c01d1b1435fd26e56
Nepsoul <nirumagar901@gmail.com>
2024-06-25 13:52:47 +0545

    chore: fetch and create the data from server feat. db file

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from "next/server"; //wrapper around response to minimize typescript issue
+import db from "@/utils/db";
 
 //response shows in localhost:3000/api/todo
 export const GET = async (request: Request) => {
-  return NextResponse.json({ message: "hello" });
+  const todos = await db.todo.findMany({});
+  return NextResponse.json({ data: todos });
 };
 
 export const POST = async (request: Request) => {
   const data = await request.json();
-  return NextResponse.json({ message: data });
+  const todo = await db.todo.create({ data });
+  return NextResponse.json({ message: todo });
 };
